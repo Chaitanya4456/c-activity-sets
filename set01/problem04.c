8bdb02bdae30c61f7a47faa61cb8ff64b516863e
chaitanya <chaitanyam.bsc23@rvu.edu.in>
2024-01-29 14:50:57 +0530

    test

--- set01/problem04.c
+++ set01/problem04.c
@@ -1,12 +1,21 @@
 #include<stdio.h>
-   void input(int *a,int *b)
+
+int main()
 {
+    int a,b,s;
+    input(a,b);
+    add(a,b,&s);
+    output(a,b,s);
+    return 0;
+}
+   void input(int *a,int *b)
+  {
     printf("enter first number: ");
     scanf("%d",a);
     printf("enter second number: ");
     scanf("%d",b);
     
-}
+  }
 
 void add(int a,int b,int *sum)
 {
@@ -15,12 +24,4 @@ void add(int a,int b,int *sum)
 void output(int a,int b,int sum)
 {
     printf("the sum of %d and %d is %d",a,b,sum);
-}
-int main()
-{
-    int a,b,s;
-    input(a,b);
-    add(a,b,&s);
-    output(a,b,s);
-    return 0;
 }

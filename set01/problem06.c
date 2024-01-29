bf7ab665beb456fba829a0787efcb27596de7366
chaitanya <chaitanyam.bsc23@rvu.edu.in>
2024-01-29 15:44:54 +0530

    test

---
+++ set01/problem06.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+void input(int *a,int *b,int *c)
+{
+    printf("enter the first number: ");
+    scanf("%d",a);
+    printf("enter the second number: ");
+    scanf("%d",b);
+}
+
+void compare(int a,int b,int c,int *largest)
+{
+    if(a>=b && a>=c)
+    *largest = a;
+    if(b>=a && b>=c)
+    *largest = b;
+    if(c>=a && c>=b)
+    *largest = c;
+}
+void output(int a,int b,int c,int largest)
+{
+    printf("largest number of %d,%d and %d is %d",a,b,c,largest);
+}
+int main()
+{
+    int a,b,c,l;
+    input(a,b,c);
+    compare(a,b,c,&l);
+    output(a,b,c,l);
+    return 0;
+}

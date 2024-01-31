5c11b45804464cbc08ea8ec77e9cfa0f3f9c08cf
chaitanya <chaitanyam.bsc23@rvu.edu.in>
2024-02-01 05:07:10 +0530

    test

--- set01/problem10.c
+++ set01/problem10.c
@@ -1,49 +1,53 @@
-#include<stdio.h>
-int input_array_size();
-void input_array(int n, int a[n]);
-int sum_n_array(int n, int a[n]);
-void output(int n, int a[n],int sum);
+#include <stdio.h>
+#include <string.h>
+
+
+void input_two_strings(char *string1, char *string2);
+int stringcompare(char *string1, char *string2);
+void output(char *string1, char *string2, int result);
+
 int main()
 {
-    int n, sum;
-    n = input_array_size();
-    int a[n]; 
-    input_array(n, a); 
-    sum = sum_n_array(n, a);
-    output(n, a, sum); 
+    int result;
+    char string1[100], string2[100];
+    input_two_strings(string1, string2);
+    result=stringcompare(string1, string2);
+    printf("result is %d\n", result);
+    output(string1, string2, result);
     return 0;
+   
 }
-int input_array_size()
+
+
+
+
+void input_two_strings(char *string1, char *string2)
 {
-    int n;
-    printf("Enter the number of elements: ");
-    scanf("%d", &n);
-    return n;
+  printf("Enter the first string\n");
+  scanf("%s[^\n]", string1);
+  printf("Enter the second string\n");
+  scanf("%s[^\n]", string2);
 }
-void input_array(int n, int a[n])
+
+
+int stringcompare(char *string1, char *string2)
 {
-    printf("Enter %d numbers: ", n);
-    for (int i = 0; i < n; i++)
-    {
-        scanf("%d", &a[i]);
-    }
+  int i=0;
+  for(i=0;string1[i]!='\0' && string2[i]!='\0' && string1[i]==string2[i];i++);
+
+  if(string1[i]==string2[i])
+    return 0;
+  if(string1[i]>string2[i])
+    return 1;
+  else return 2;
 }
-int sum_n_array(int n, int a[n])
+
+void output(char *string1, char *string2, int result)
 {
-    int sum = 0;
-    for (int i = 0; i < n; i++)
-    {
-        sum += a[i];
-    }
-    return sum;
-}
-void output(int n, int a[n],int sum)
-{
-    printf("The numbers entered are: ");
-    for (int i = 0; i < n; i++)
-    {
-        printf("%d ", a[i]);
-    }
-    printf("\n");
-    printf("The sum of these numbers is: %d\n", sum);
+  if(result==0)
+    printf("%s is equal to %s\n", string1, string2);
+  else if(result==1)
+    printf("%s is greater than %s\n", string1, string2);
+  else if(result==2)
+    printf("%s is greater than %s\n", string2, string1);
 }

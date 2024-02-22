26e33cc6b0c5dda89ed70a7d57f8292b01c184d8
chaitanya <chaitanyam.bsc23@rvu.edu.in>
2024-02-22 21:04:13 +0530

    test

--- set01/problem02.c
+++ set01/problem02.c
@@ -2,9 +2,9 @@
 int main()
 {
     int c,d,sum;
-    printf("enter a number: ");
+    printf("enter the first number: ");
     scanf("%d",&c);
-    printf("enter a number: ");
+    printf("enter the second number: ");
     scanf("%d",&d);
     sum = c+d;
     printf("the sum of %d and %d is %d",c,d,sum);

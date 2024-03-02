08002ba32cbc8ae8f9297cbdc1a531bf222253b8
chaitanya <chaitanyam.bsc23@rvu.edu.in>
2024-03-02 18:10:58 +0530

    test

--- set03/problem02.c
+++ set03/problem02.c
@@ -13,13 +13,13 @@ int main() {
     return 0;
 }
 
-void input_triangle(float *x1, float *y1, float *x2, float *y2, float *x3, float *y3) {
+void input_triangle(float *x1, float *y1, float *x2, float *y2, float *x4, float *y4) {
     printf("Enter coordinates of first point (x1 y1): ");
     scanf("%f %f", x1, y1);
     printf("Enter coordinates of second point (x2 y2): ");
     scanf("%f %f", x2, y2);
-    printf("Enter coordinates of third point (x3 y3): ");
-    scanf("%f %f", x3, y3);
+    printf("Enter coordinates of third point (x4 y4): ");
+    scanf("%f %f", x4, y4);
 }
 
 int is_triangle(float x1, float y1, float x2, float y2, float x3, float y3) {
